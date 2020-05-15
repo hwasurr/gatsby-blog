5d7b5bbaf79d97438cbb860e99d6ae166e6aaae5
hwasurr <iamsupermazinga@gmail.com>
2020-05-15 13:13:58 +0900

    cdk 코드 정리

--- aws-cdk/lib/hwasurr-dot-io-aws-stack.ts
+++ aws-cdk/lib/hwasurr-dot-io-aws-stack.ts
@@ -8,21 +8,34 @@ export default class HwasurrDotIoAwsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Get environment variables from ssm
+    const REPOSITORY_OWNER = 'hwasurr';
+    const REPOSITORY_NAME = 'hwasurr.io';
+    const DOMAIN_NAME = 'hwasurr.io';
+    const devAccessId = 'dev';
+    const devAccessPassword = 'dev';
+
+
     // Define Amplify console app
     const myAmplifyApp = new amplify.App(this, 'hwasurr-dot-io', {
       sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
-        owner: 'hwasurr',
-        repository: 'hwasurr.io',
+        owner: REPOSITORY_OWNER,
+        repository: REPOSITORY_NAME,
         oauthToken: cdk.SecretValue.plainText(process.env.GITHUB_ACCESS_TOKEN_HWASURR_DOT_IO!)
       })
     });
 
     // Add master branchs
     const myAmplifyAppMasterBranch = myAmplifyApp.addBranch('master');
-    const myAmplifyAppDevBranch = myAmplifyApp.addBranch('dev');
+    const myAmplifyAppDevBranch = myAmplifyApp.addBranch('dev', {
+      basicAuth: amplify.BasicAuth.fromCredentials( // Restricting Access
+        devAccessId,
+        cdk.SecretValue.plainText(devAccessPassword)
+      )
+    });
 
     // Add domains
-    const domain = myAmplifyApp.addDomain('hwasurr.io');
+    const domain = myAmplifyApp.addDomain(DOMAIN_NAME);
     domain.mapRoot(myAmplifyAppMasterBranch);
     domain.mapSubDomain(myAmplifyAppDevBranch); // Sub domain prefix defaults to branch name
   }
