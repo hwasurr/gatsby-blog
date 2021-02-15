454fdfe3724f721901a9c1eaf48f6efeaac92d1a
iamsupermazinga <iamsupermazinga@gmail.com>
2021-02-15 20:13:19 +0900

    프로필 사진 수정

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -14,7 +14,7 @@ import { rhythm } from '../utils/typography';
 const Bio = (): JSX.Element => {
   const data = useStaticQuery(graphql`
     query BioQuery {
-      avatar: file(absolutePath: { regex: "/profile-pic.png/" }) {
+      avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
         childImageSharp {
           fixed(width: 75, height: 75) {
             ...GatsbyImageSharpFixed
